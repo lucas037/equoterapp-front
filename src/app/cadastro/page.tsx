292053c363e576d8aa85de8a14394332eafac6b2
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-08-20 00:04:36 -0300

    fix: Responsividade aplicada na Etapa 1

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import Header from "@/components/Header";
@@ -9,17 +9,17 @@ import Etapa3 from "./Etapa3";
 import Etapa4 from "./Etapa4";
 
 export default function Cadastro() {
+  const [dadosPaciente, setDadosPaciente] = useState<DadosPaciente>({} as DadosPaciente);
+  const [etapa, setEtapa] = useState(1);
+
   function handleClick() {
     window.location.href = "/login";
   }
 
   function cadastroFinalizado() {
-    alert("Cadastro Finalizado! "+dadosPaciente.nome);
+    alert("Cadastro Finalizado! " + dadosPaciente.nome);
     window.location.href = "/login";
   }
-
-  const [dadosPaciente, setDadosPaciente] = useState<DadosPaciente>({} as DadosPaciente);
-  const [etapa, setEtapa] = useState(1);
 
   function changeDadosPaciente(dadosPaciente: DadosPaciente) {
     setDadosPaciente(dadosPaciente);
@@ -35,52 +35,68 @@ export default function Cadastro() {
 
   return (
     <div className="h-screen w-full flex flex-col items-center">
-      <Header buttonName="Login" handleClick={handleClick}/>
+      <Header buttonName="Login" handleClick={handleClick} />
 
+      <div className="h-[80vh] w-[90%] xl:w-[80%] flex flex-col xl:flex-row overflow-hidden">
         {etapa === 1 && (
-          <div className='h-[75%] w-[80%] flex'>
-            <img 
-              src="/assets/adulto.png"
-              alt="foto"
-              className="w-auto h-full opacity-50 border border-[#C3C3C3] hidden xl:flex xl:max-w-[100%]"
-            />
-            <Etapa1
-            dadosPaciente={dadosPaciente}
-            changeDadosPaciente={changeDadosPaciente}
-            clickProximaEtapa={clickProximaEtapa}
-            />
+          <div className="flex flex-col xl:flex-row w-full h-full">
+            <div className="relative w-full xl:w-1/2 h-full flex-shrink-0 hidden xl:block">
+              <img
+                src="/assets/adulto.png"
+                alt="foto"
+                className="absolute inset-0 w-full h-full object-cover opacity-50"
+                style={{ width: '100%', height: '100%' }}
+              />
+            </div>
+            <div className="w-full xl:w-1/2 h-full flex items-center bg-white p-4 overflow-y-auto">
+              <Etapa1
+                dadosPaciente={dadosPaciente}
+                changeDadosPaciente={changeDadosPaciente}
+                clickProximaEtapa={clickProximaEtapa}
+              />
+            </div>
           </div>
         )}
 
         {etapa === 2 && (
-          <div className='h-[75%] w-[80%] flex'>
-            <img 
-              src="/assets/adulto.png"
-              alt="foto"
-              className="w-auto h-full opacity-50 border border-[#C3C3C3] hidden xl:flex xl:max-w-[100%]"
-            />
-            <Etapa2
-            dadosPaciente={dadosPaciente}
-            changeDadosPaciente={changeDadosPaciente}
-            clickProximaEtapa={clickProximaEtapa}
-            clickEtapaAnterior={clickEtapaAnterior}
-            />
+          <div className="flex flex-col xl:flex-row w-full h-full">
+            <div className="relative w-full xl:w-1/2 h-full flex-shrink-0 hidden xl:block">
+            <img
+                src="/assets/adulto.png"
+                alt="foto"
+                className="absolute inset-0 w-full h-full object-cover opacity-50"
+                style={{ width: '100%', height: '100%' }}
+              />
+            </div>
+            <div className="w-full xl:w-1/2 h-full flex items-center bg-white p-4 overflow-y-auto">
+              <Etapa2
+                dadosPaciente={dadosPaciente}
+                changeDadosPaciente={changeDadosPaciente}
+                clickProximaEtapa={clickProximaEtapa}
+                clickEtapaAnterior={clickEtapaAnterior}
+              />
+            </div>
           </div>
         )}
 
         {etapa === 3 && (
-          <div className='h-[75%] w-[80%] flex'>
-            <img 
-              src="/assets/adulto.png"
-              alt="foto"
-              className="w-auto h-full opacity-50 border border-[#C3C3C3] hidden xl:flex xl:max-w-[100%]"
-            />
-            <Etapa3
-            dadosPaciente={dadosPaciente}
-            changeDadosPaciente={changeDadosPaciente}
-            clickProximaEtapa={clickProximaEtapa}
-            clickEtapaAnterior={clickEtapaAnterior}
-            />
+          <div className="flex flex-col xl:flex-row w-full h-full">
+            <div className="relative w-full xl:w-1/2 h-full flex-shrink-0 hidden xl:block">
+              <img
+                src="/assets/adulto.png"
+                alt="foto"
+                className="absolute inset-0 w-full h-full object-cover opacity-50"
+                style={{ width: '100%', height: '100%' }}
+              />
+            </div>
+            <div className="w-full xl:w-1/2 h-full flex items-center bg-white p-4 overflow-y-auto">
+              <Etapa3
+                dadosPaciente={dadosPaciente}
+                changeDadosPaciente={changeDadosPaciente}
+                clickProximaEtapa={clickProximaEtapa}
+                clickEtapaAnterior={clickEtapaAnterior}
+              />
+            </div>
           </div>
         )}
 
@@ -91,5 +107,6 @@ export default function Cadastro() {
           clickEtapaAnterior={clickEtapaAnterior}
         />)}
       </div>
-  )
+    </div>
+  );
 }
