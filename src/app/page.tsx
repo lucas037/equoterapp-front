f580f960c8fbb1088fdaf1ada93084d687316278
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-08-20 00:33:52 -0300

    refactor: Update SlideTxt and SlideInfo components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
-"use client"
+"use client";
 
 import SlideTxt from '@/components/SlideTxt';
 import Footer from '../components/Footer';
-import Header2 from '../components/Header2';
+import Header from '../components/Header';
 import SlideImg from '../components/SlideImg';
 import SlideDocumentos from '@/components/SlideDocumentos';
 import SlideInfo from '@/components/SlideInfo';
-import Header from '@/components/Header';
 import { motion } from 'framer-motion';
+
+const handleNavigation = () => {
+  window.location.href = '/login'; 
+};
 
 export default function Home() {
   const images = [
-    { image: '/assets/Capturar.png', alt: 'Imagem Principal', nameDocument: 'Documento 1' },
+    { image: '/assets/Capturar.png', alt: 'Imagem Principal', nameDocument: 'Documento1' },
     { image: '/assets/Capturar2.png', alt: 'Imagem Principal2', nameDocument: 'Documento 2' },
     { image: '/assets/Capturar3.png', alt: 'Imagem Principal', nameDocument: 'Documento 3' },
     { image: '/assets/Capturar4.png', alt: 'Imagem Principal2', nameDocument: 'Documento 4' },
@@ -19,10 +22,6 @@ export default function Home() {
     { image: '/assets/Capturar6.png', alt: 'Imagem Principal2', nameDocument: 'Documento 6' },
     { image: '/assets/Capturar7.png', alt: 'Imagem Principal', nameDocument: 'Documento 7' },
   ];
-
-  function handleClick() {
-    window.location.href = "/login";
-  }
 
   return (
     <motion.div
@@ -33,60 +32,54 @@ export default function Home() {
       id="page-transition"
       className="flex flex-col min-h-screen"
     >
-      <div className="flex flex-col min-h-screen">
+      <Header buttonName='Entrar' handleClick={handleNavigation} />
 
-        <Header buttonName='Login' handleClick={handleClick} />
+      <main className="flex-grow mt-2">
+        <SlideImg />
 
-        <div className="flex-grow">
-          <SlideImg />
-        </div>
-
-        <div className="flex flex-col lg:flex-row h-full justify-between w-full px-4 lg:px-20 space-y-4 lg:space-y-0 lg:space-x-4">
-          <div className="lg:w-1/2 h-full flex items-center justify-center">
-
-            <SlideTxt
-              title="Quem somos nós?"
-              slides={[
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry...",
-                "There are many variations of passages of Lorem Ipsum available...",
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry...",
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry..."
-              ]}
-            />
+        <section className="flex flex-col lg:flex-row h-full justify-between w-full px-4 lg:px-20 space-y-4 lg:space-y-0 lg:space-x-4 mt-20">
+        <div className="lg:w-1/2 h-full flex items-center justify-center">
+        <SlideTxt
+      title="Quem somos nós?"
+      slides={[
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry...",
+        "There are many variations of passages of Lorem Ipsum available...",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry...",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry..."
+      ]}
+    />
           </div>
-          <div className="lg:w-1/2 h-full flex items-center justify-center">
-            <SlideTxt
-              title="Regras"
-              slides={[
-                "It is a long established fact that a reader will be distracted...",
-                "There are many variations of passages of Lorem Ipsum available...",
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry...",
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
-              ]}
-            />
+          <div className="lg:w-1/2">
+          <SlideTxt
+      title="Regras"
+      slides={[
+        "It is a long established fact that a reader will be distracted...",
+        "There are many variations of passages of Lorem Ipsum available...",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry...",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry..."
+      ]}
+    />
           </div>
-        </div>
+        </section>
 
-        <div className="flex flex-col space-y-10 lg:space-y-0">
-          <div className="h-full px-4 lg:px-20">
-            <SlideInfo
-              title={'Como funciona'}
-              slides={[
-                { text1: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..', text2: 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.' },
-                { text1: 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', text2: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..' },
-              ]}
-            />
+        <section className="flex flex-col space-y-10 lg:space-y-0 mt-10">
+          <div className="px-4 lg:px-20">
+          <SlideInfo
+            title={'Como funciona'}
+            slides={[
+              { text1: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..', text2: 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.' },
+              { text1: 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', text2: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard' },
+            ]}
+          />
           </div>
 
-          <div className="h-full px-4 lg:px-20 mt-20">
+          <div className="px-4 lg:px-20 pt-20">
             <SlideDocumentos title='Documentos' images={images} />
           </div>
-        </div>
+        </section>
+      </main>
 
-        <div className='mt-20'>
-          <Footer />
-        </div>
-      </div>
-      <motion.div />
-     );
+      <Footer />
+    </motion.div>
+  );
 }
