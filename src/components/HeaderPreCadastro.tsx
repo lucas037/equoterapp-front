f76b71e88242bc9564c468583ed45201b4fcb4ee
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-09-02 19:54:02 -0300

    update: Colocando condicionais para modificar o status e mensagem de aprovação

--- src/components/HeaderPreCadastro.tsx
+++ src/components/HeaderPreCadastro.tsx
@@ -17,22 +17,19 @@ export default function Header(props: InterfaceProps) {
 
     return (
 
-        <div className="w-full h-[120px] flex justify-center mb-4 bg-[#4B8A89] rounded-md">
+        <div className="w-[80%] h-[120px] flex justify-center mb-4 bg-[#4B8A89] rounded-md">
             <div className='w-[90%] h-[120px] flex justify-between items-center'>
-
-
-
                 <div className='text-xl flex flex-col xl:flex xl:max-w-[100%] text-white'>
                     <div>Olá Maria,</div>
                     <div>Seu pré-cadastro no sistema se encontra</div>
                 </div>
 
                 <div className='text-xl flex flex-col xl:flex xl:max-w-[100%] text-white relative'>
-                    <Botao className='w-[140px] h-[50px] rounded-md flex justify-center items-center bg-white text-teal-600 font-extrabold'>
+                    <Botao className={`w-[180px] h-[70px] rounded-md flex justify-center items-center ${props.buttonName === 'Aprovado' ? 'bg-white text-teal-600 font-extrabold hover:bg-teal-500 hover:text-white' : props.buttonName === 'Reprovado' ? 'bg-white text-red-800 font-extrabold hover:bg-red-600 hover:text-white' : 'bg-white text-[#4B8A89] font-extrabold hover:bg-teal-800 hover:text-white'} `}>
                         {props.buttonName}
                     </Botao>
                     <div
-                        className='w-10 h-10 rounded-full bg-teal-400 flex items-center justify-center cursor-pointer shadow-lg absolute top-5 left-28'
+                        className='w-10 h-10 rounded-full bg-teal-400 flex items-center justify-center cursor-pointer shadow-lg absolute top-10 left-36'
                         onMouseEnter={() => setShowTooltip(true)}
                         onMouseLeave={() => setShowTooltip(false)}
                     >
@@ -48,7 +45,12 @@ export default function Header(props: InterfaceProps) {
                             style={{ whiteSpace: 'normal', overflowWrap: 'break-word' }}
                         >
                             <p className='text-sm leading-tight'>
-                                Isso significa que seu cadastro foi aprovado e você já pode concluir seu cadastro no sistema.
+                                {props.buttonName === 'Aprovado' ?
+                                    'Isso significa que seu cadastro foi aprovado e você já pode concluir seu cadastro no sistema.' :
+                                    props.buttonName === 'Reprovado' ?
+                                        'Isso significa que seu cadastro foi reprovado. Por favor, entre em contato com o suporte para mais informações.' :
+                                        'Seu cadastro ainda não foi aprovado. Por favor, aguarde a aprovação para concluir seu cadastro no sistema.'
+                                }
                             </p>
                         </motion.div>
                     )}
