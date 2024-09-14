0c6fb2a20052b81c7db40f7be7a8f8ac1606d19d
BEN ARIEL FRANÇA MARTINS <fadomarshemello@gmail.com>
2024-09-14 13:06:27 -0300

    deixar renda e mebros lado a lado

--- src/app/gerenciar-precadastro/componentes/InfoDoc.tsx
+++ src/app/gerenciar-precadastro/componentes/InfoDoc.tsx
@@ -1,26 +1,37 @@
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-interface InfoDocProps {
-    nome: string;
-    nMembros : number;
-    rendaM : number;
-    handleNavigation: () => void;
-}
-export default function InfoDoc( {nome, nMembros, rendaM,handleNavigation} : InfoDocProps) {
-    return (
-        <div className="border-2 border-black rounded-md w-full h-full p-2"> 
 
-            <div className="flex flex-row">
-                <div className="flex flex-col text-[#255A59] w-[70%] justify-between">
-                <h2>{nome.toUpperCase()}</h2>
-                    <div>
-                        <div>Membros: {nMembros}</div>
-                        <div>Renda Mensal: {rendaM}</div>
-                    </div>
-                </div>
-                <div className="w-[30%] h-full border-2 border-[#255A59] p-2">
-                    <button onClick={handleNavigation}><ArrowForwardIosIcon className='text-[#255A59] h-full w-full '/> </button>
-                </div>
+interface InfoDocProps {
+  nome: string;
+  nMembros: number;
+  rendaM: number;
+  handleNavigation: () => void;
+}
+
+export default function InfoDoc({ nome, nMembros, rendaM, handleNavigation }: InfoDocProps) {
+  return (
+    <div className="border-2 border-black rounded-md w-full h-full p-2">
+      <div className="flex flex-row">
+        <div className="flex flex-col text-[#255A59] w-[70%] justify-between">
+          <h2>{nome.toUpperCase()}</h2>
+          <div className="flex flex-row gap-x-4">
+            {/* Coluna de número de membros */}
+            <div className="flex flex-col items-center">
+              <p>{nMembros}</p>
+              <p>membros</p>
             </div>
+            {/* Coluna de renda mensal */}
+            <div className="flex flex-col items-center">
+              <p>R${rendaM}</p>
+              <p>renda mensal</p>
             </div>
-            );
+          </div>
+        </div>
+        <div className="w-[30%] h-full border-2 border-[#255A59] p-2">
+          <button onClick={handleNavigation}>
+            <ArrowForwardIosIcon className="text-[#255A59] h-full w-full" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
