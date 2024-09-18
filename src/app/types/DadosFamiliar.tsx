9c9d5be8129fd0deca6217cb847c0a37a69d2eb3
lucas037 <lugab037@gmail.com>
2024-09-18 12:00:02 -0300

    feat: Adicionar tipo 'familiar do paciente'

---
+++ src/app/types/DadosFamiliar.tsx
@@ -0,0 +1,13 @@
+export default interface DadosFamiliar {
+    nome: string,
+    cpf: string,
+    telefone: string,
+    email: string,
+    senha: string,
+    sexo: string,
+    escolaridade: string,
+    localTrabalho: string,
+    ocupacao: string,
+    telefoneTrabalho: string,
+    salario: number
+}
