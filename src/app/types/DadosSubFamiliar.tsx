301fb03b5e3ae8fc2fb28018d46f8aae1141902f
lucas037 <lugab037@gmail.com>
2024-09-30 22:07:34 -0300

    feat: Criar tipo SubFamiliar.

---
+++ src/app/types/DadosSubFamiliar.tsx
@@ -0,0 +1,8 @@
+export default interface DadosSubFamiliar {
+    name: string
+    familiarKinship: string
+    occupation: string
+    scholarity: string
+    income: number
+    patientId: number
+}
