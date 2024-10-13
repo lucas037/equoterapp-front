105537996b5587cfc33579a4e074c93f3c6f1eac
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-10-13 19:07:28 -0300

    refactor: Update handleNavigation function in InfoSec component

--- src/app/gerenciar-sessoes/componentes/InfoSec.tsx
+++ src/app/gerenciar-sessoes/componentes/InfoSec.tsx
@@ -2,15 +2,23 @@ import { Person, PersonTwoTone } from '@mui/icons-material';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 interface InfoSecProps {
+    idsessao: number;
     nome: string;
     mediador: string;
     quantSessoes: number;
     presenca: boolean;
-    handleNavigation: () => void;
     icon?: React.ReactElement;
 }
 
-export default function InfoSec({ nome, icon, quantSessoes, handleNavigation, presenca, mediador }: InfoSecProps) {
+const handleNavigation = (idsessao: number, idpaciente: number) => {
+    if (idsessao && idpaciente) {
+        window.location.href = `/ver-sessao?idpaciente=${idpaciente}&idsessao=${idsessao}`;
+    } else {
+        console.error("ID da sessão ou ID do paciente não está definido");
+    }
+};
+
+export default function InfoSec({ idsessao, nome, icon, quantSessoes, presenca, mediador }: InfoSecProps) {
     return (
         <div className="border-2 border-black rounded-md h-full p-4">
             <div className="flex flex-row">
@@ -61,7 +69,7 @@ export default function InfoSec({ nome, icon, quantSessoes, handleNavigation, pr
                 </div>
 
                 <div className="flex flex-col text-[#255A59] w-[45%] justify-center items-center mt-4">
-                    <button onClick={() => window.location.href = '/ver-sessao'} className="flex items-center gap-x-2 text-sm p-2 justify-center bg-green-950 text-white rounded-md">
+                <button onClick={() => handleNavigation(idsessao, 1)} className="flex items-center gap-x-2 text-sm p-2 justify-center bg-green-950 text-white rounded-md">
                         Ver Detalhes
                     </button>
                 </div>
