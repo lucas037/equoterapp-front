4352667f78039c080a3ec169686c4a0a87ddd9d7
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-10-13 04:27:24 -0300

    update: Melhoria visual da interface. correção de card, da responsividade e dos botões.

--- src/app/verificar-cadastros/page.tsx
+++ src/app/verificar-cadastros/page.tsx
@@ -1,12 +1,15 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import requestsAuth from '../../utils/requestsAuth';
 import requestsFamiliar from '../../utils/requestsFamiliar';
 import requestsPatient from '../../utils/requestsPatient';
 import requestsSubFamiliar from '../../utils/requestsSubFamiliar';
 import Header from "@/components/Header";
 import DadosSubFamiliar from "@/types/DadosSubFamiliar";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { Person, Person3 } from "@mui/icons-material";
+import { Botao } from "@/components/Botao";
 
 export default function VericarCadastros() {
     const [names, setNames] = useState<string[]>([]);
@@ -15,7 +18,7 @@ export default function VericarCadastros() {
 
     const [namePatient, setNamePatient] = useState("");
     const [subFamiliars, setSubFamiliars] = useState<DadosSubFamiliar[]>([]);
-    
+
     useEffect(() => {
         if (requestsAuth.getPosition() === "") {
             window.location.href = "/login";
@@ -31,7 +34,7 @@ export default function VericarCadastros() {
     }, []);
 
     async function activeModal(index: number) {
-        setIndex(index)
+        setIndex(index);
 
         await requestsPatient.getPatient(ids[index]);
 
@@ -43,93 +46,90 @@ export default function VericarCadastros() {
 
     async function changeStatus(newStatus: boolean) {
         await requestsFamiliar.approvePreCadastro(ids[index], newStatus);
-        window.location.href = "/verificar-cadastros"
+        window.location.href = "/verificar-cadastros";
     }
 
     return (
         <div className="h-screen flex flex-col">
             <Header
-            buttonName="Sair"
-            handleClick={() => {window.location.href = "/login"}}
-            buttonsNames={["Verificar Documentos", "Verificar Cadastros", "Gerenciamento", "Perfil"]}
+                buttonName="Sair"
+                handleClick={() => { window.location.href = "/login"; }}
+                buttonsNames={["Verificar Documentos", "Verificar Cadastros", "Gerenciamento", "Perfil"]}
             />
 
-            <div className="w-full h-full flex justify-center items-center">
-                
-                <div className="min-h-[96%] w-[96%] border border-black p-8 flex justify-center">
-
+            <div className="w-full h-full px-20">
+                <div className="w-full h-full border-2 border-black p-4">
                     {ids.length <= 0 && <div className="w-full flex justify-center items-center text-sm font-bold">Nenhum pré-cadastro pendente.</div>}
 
-                    { index < 0 ?
-
-                        <div className="w-[90%] h-full inline-flex flex-wrap gap-4">
-
+                    {index < 0 ? (
+                        <div className="w-full h-full inline-flex flex-wrap gap-4">
                             {names.map((name, index) => (
-                                <div onClick={() => {activeModal(index)}} key={index} className="w-[350px] h-[100px] border border-[#255A59] rounded-2xl p-4 font-bold text-[#255A59] flex justify-between cursor-pointer">
-                                    <div className="uppercase">{name}</div>
-                                    <div className="w-[60px] h-[60px] border border-4 border-[#4B8A89] text-[#4B8A89] rounded-xl flex justify-center items-center">
-                                        <div className="text-5xl">{">"}</div>
+                                <div onClick={() => { activeModal(index); }} key={index} className="border-2 border-black rounded-md w-[350px] h-[100px] p-4 flex flex-col justify-between cursor-pointer">
+                                    <div className="flex flex-row justify-between items-center">
+                                        <div className="flex flex-col text-[#255A59] w-[70%]">
+                                            <div className="flex items-center gap-x-2">
+                                                <Person />
+                                                <h2 className="uppercase">{name}</h2>
+                                            </div>
+                                            <div className="flex flex-row gap-x-4 mt-2">
+                                                <div className="flex flex-col items-center">
+                                                    <p className='text-xs'>Parentesco</p>
+                                                    <p className='text-xs'>Profissão</p>
+                                                </div>
+                                                <div className="flex flex-col items-center">
+                                                    <p className='text-xs'>Escolaridade</p>
+                                                    <p className='text-xs'>Renda</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="w-[60px] h-[60px] border-2 border-[#4B8A89] rounded-md flex justify-center items-center">
+                                            <ArrowForwardIosIcon className="text-[#4B8A89] w-[50%] h-[50%]" />
+                                        </div>
                                     </div>
                                 </div>
                             ))}
-
-
                         </div>
-                            
-
-                        :
-
-                        <div className="w-full h-[30px] flex flex-col items-center gap-8">
-
-                            <div className="w-[96%] p-2 flex justify-between bg-[#4B8A89] text-white uppercase text-sm">
-                                <div>VERIFICAÇÃO DE PRÉ-CADASTRO</div>
-                                <div>Paciente: {namePatient} / Responsável: {names[index]}</div>
+                    ) : (
+                        <div className="w-full flex flex-col items-center gap-8">
+                            <div className="w-[100%] p-2 flex justify-between bg-[#4B8A89] text-white uppercase text-sm">
+                                <div className="ml-5">VERIFICAÇÃO DE PRÉ-CADASTRO</div>
+                                <div className="mr-5">Paciente: {namePatient} / Responsável: {names[index]}</div>
                             </div>
 
                             <div className="w-full flex flex-col gap-4 uppercase">
-                                <div className="flex font-bold">
-                                    <div className="w-[25%] flex justify-center">NOME</div>
-                                    <div className="w-[15%] flex justify-center">PARENTESCO</div>
-                                    <div className="w-[20%] flex justify-center">PROFISSÃO</div>
-                                    <div className="w-[25%] flex justify-center">ESCOLARIDADE</div>
-                                    <div className="w-[15%] flex justify-center">RENDA</div>
+                                <div className="flex font-bold flex-wrap">
+                                    <div className="flex-1 min-w-[100px] flex justify-center">NOME</div>
+                                    <div className="flex-1 min-w-[100px] flex justify-center">PARENTESCO</div>
+                                    <div className="flex-1 min-w-[100px] flex justify-center">PROFISSÃO</div>
+                                    <div className="flex-1 min-w-[100px] flex justify-center">ESCOLARIDADE</div>
+                                    <div className="flex-1 min-w-[100px] flex justify-center">RENDA</div>
                                 </div>
 
-                                {
-                                    subFamiliars.map((subFamiliar, index) => (
-                                        <div key={subFamiliar.patientId || index} className="flex flex-col gap-4">
-                                            <div className="flex text-[#65ADAC]">
-                                                <div className="w-[25%] flex justify-center items-center">{subFamiliar.name}</div>
-                                                <div className="w-[15%] flex justify-center items-center">{subFamiliar.familiarKinship}</div>
-                                                <div className="w-[20%] flex justify-center items-center">{subFamiliar.occupation}</div>
-                                                <div className="w-[25%] flex justify-center items-center">{subFamiliar.scholarity}</div>
-                                                <div className="w-[15%] flex justify-center items-center">R${subFamiliar.income}</div>
-                                            </div>
-                                        </div>
-                                    ))
-                                }
-
-
+                                {subFamiliars.map((subFamiliar, index) => (
+                                    <div key={subFamiliar.patientId || index} className="flex flex-wrap text-[#65ADAC]">
+                                        <div className="flex-1 min-w-[100px] flex justify-center items-center">{subFamiliar.name}</div>
+                                        <div className="flex-1 min-w-[100px] flex justify-center items-center">{subFamiliar.familiarKinship}</div>
+                                        <div className="flex-1 min-w-[100px] flex justify-center items-center">{subFamiliar.occupation}</div>
+                                        <div className="flex-1 min-w-[100px] flex justify-center items-center">{subFamiliar.scholarity}</div>
+                                        <div className="flex-1 min-w-[100px] flex justify-center items-center">R${subFamiliar.income}</div>
+                                    </div>
+                                ))}
                             </div>
-                            
-                            <div className="flex gap-4">
-                                <div onClick={() => {changeStatus(true)}} className="p-4 bg-red-400 cursor-pointer">
-                                    <div>APROVAR PRÉ-CADASTRO</div>
-                                </div>
 
-                                <div onClick={() => {changeStatus(false)}} className="p-4 bg-red-400 cursor-pointer">
+                            <div className="flex gap-4 mt-6 w-full items-center justify-center">
+
+                                <Botao onClick={() => { changeStatus(true); }} className='w-[40%] h-[50px] rounded-md flex justify-center items-center'>
+                                    Aprovar Pré Cadastro
+                                </Botao>
+
+                                <div onClick={() => { changeStatus(false); }} className=" w-[40%] h-[50px] flex text-center justify-center items-center bg-red-800 cursor-pointer text-white rounded-md">
                                     <div>REPROVAR PRÉ-CADASTRO</div>
                                 </div>
                             </div>
-
                         </div>
-
-
-                        
-
-                    }
+                    )}
                 </div>
             </div>
         </div>
-    )
+    );
 }
