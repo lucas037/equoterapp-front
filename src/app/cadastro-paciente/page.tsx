29528aadc8a82508142dd9db6022199c469acd91
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-10-13 02:22:13 -0300

    feat: Mascaras aplicadas e conversão da data no envio ao BackEnd

--- src/app/cadastro-paciente/page.tsx
+++ src/app/cadastro-paciente/page.tsx
@@ -10,6 +10,7 @@ import Dropdown from "@/components/Dropdown";
 import DropdownOption from "../../types/DropdownOption";
 import Header from '@/components/Header';
 import DadosPaciente from '../../types/DadosPaciente';
+import { aplicarMascaraCPF } from '@/app/cadastro/page';
 
 export default function Page() {
     const [dadosPaciente, setDadosPaciente] = useState<DadosPaciente>({} as DadosPaciente);
@@ -21,13 +22,28 @@ export default function Page() {
     }, []);
 
     async function clickProximaEtapa() {
-        await requestsPatient.registerPatient(dadosPaciente);
+
+        if (!dadosPaciente.name || !dadosPaciente.cpf || !dadosPaciente.birthDate || !dadosPaciente.sexo || !dadosPaciente.birthDate) {
+            setErro('Por favor, preencha todos os campos.');
+            return;
+        }
+
+        // Converte a data para o formato americano
+        const [day, month, year] = dadosPaciente.birthDate.split('/');
+        const birthDateAmericanFormat = `${year}-${month}-${day}`;
+
+        const pacienteDataToSend = {
+            ...dadosPaciente,
+            birthDate: birthDateAmericanFormat, // Substitui a data
+        };
+
+        await requestsPatient.registerPatient(pacienteDataToSend);
 
         if (requestsPatient.messageError != "")
             setErro(requestsPatient.messageError)
         else {
             currentPageStorage.changePage(2);
-            window.location.href = "/"+currentPageStorage.getPage();
+            window.location.href = "/" + currentPageStorage.getPage();
         }
     }
 
@@ -37,15 +53,15 @@ export default function Page() {
 
     function handleChangeNome(value: string) {
         changeDadosPaciente({
-          ...dadosPaciente,
-          name: value,
+            ...dadosPaciente,
+            name: value,
         });
     }
 
     function handleChangeCPF(value: string) {
         changeDadosPaciente({
             ...dadosPaciente,
-            cpf: value,
+            cpf: aplicarMascaraCPF(value),
         });
     }
 
@@ -57,10 +73,36 @@ export default function Page() {
     }
 
     function handleChangeBirthdate(value: string) {
+        // Remove caracteres não numéricos
+        const somenteNumeros = value.replace(/\D/g, '');
+
+        // Inicializa a variável de valor formatado
+        let formattedValue = '';
+
+        // Aplica a máscara se houver dígitos suficientes
+        if (somenteNumeros.length > 0) {
+            formattedValue += somenteNumeros.substring(0, 2); // Dia
+        }
+        if (somenteNumeros.length > 2) {
+            formattedValue += '/' + somenteNumeros.substring(2, 4); // Mês
+        }
+        if (somenteNumeros.length > 4) {
+            formattedValue += '/' + somenteNumeros.substring(4, 8); // Ano
+        }
+
+        // Atualiza o estado com a data formatada
         changeDadosPaciente({
             ...dadosPaciente,
-            birthDate: value,
+            birthDate: formattedValue,
         });
+
+        // Validação da data
+        const regex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/(\d{4})$/;
+        if (regex.test(formattedValue)) {
+            console.log("Data de nascimento válida:", formattedValue);
+        } else {
+            console.error("Data de nascimento inválida. Use o formato DD/MM/AAAA.");
+        }
     }
 
     function handleChangeIsStudent(value: string) {
@@ -122,12 +164,15 @@ export default function Page() {
     }
 
     function handleChangeNumeroNis(value: string) {
-        changeDadosPaciente({
-            ...dadosPaciente,
-            nis: value,
-        });
+        const somenteNumeros = value.replace(/\D/g, '');
+        if (somenteNumeros.length <= 11) {
+            changeDadosPaciente({
+                ...dadosPaciente,
+                nis: somenteNumeros,
+            });
+        }
     }
-    
+
     const isStudentOptions: DropdownOption[] = [
         { label: "Selecionar", value: "" },
         { label: "Sim", value: "s" },
@@ -149,23 +194,23 @@ export default function Page() {
     return (
         <div className="">
             <Header
-            buttonName="Sair"
-            handleClick={() => {
-                tokenStorage.clearToken();
-                currentPageStorage.clearCurrentPage();
-                window.location.href = "/login"
-            }}
-            buttonsNames={["Quem Somos", "Regras", "Contatos", "Documentos"]}
+                buttonName="Sair"
+                handleClick={() => {
+                    tokenStorage.clearToken();
+                    currentPageStorage.clearCurrentPage();
+                    window.location.href = "/login"
+                }}
+                buttonsNames={["Quem Somos", "Regras", "Contatos", "Documentos"]}
             />
 
             <div className="w-full sm:h-[calc(100vh-160px)] flex justify-center items-center">
                 <div className="w-[95%] sm:h-[90%] border flex border-gray-400 mb-3" >
                     <Image
-                    src = "/assets/adulto.png"
-                    alt = ""
-                    height={288}
-                    width={410}
-                    className="hidden lg:block h-full w-auto opacity-50"
+                        src="/assets/adulto.png"
+                        alt=""
+                        height={288}
+                        width={410}
+                        className="hidden lg:block h-full w-auto opacity-50"
                     />
 
                     <div className="w-full h-full flex flex-col justify-around items-center">
@@ -180,109 +225,109 @@ export default function Page() {
                         <div className="w-[90%] text-sm font-bold flex flex-col items-center gap-3">
                             <div className="w-full flex flex-col gap-3 sm:flex-row sm:justify-between">
                                 <Input
-                                name={"NOME"}
-                                style={"w-[100%] sm:w-[66%]"}
-                                value={dadosPaciente.name}
-                                onChange={handleChangeNome}
-                                height="h-[45px]"
+                                    name={"NOME"}
+                                    style={"w-[100%] sm:w-[66%]"}
+                                    value={dadosPaciente.name}
+                                    onChange={handleChangeNome}
+                                    height="h-[45px]"
                                 />
 
                                 <Input
-                                name={"CPF"}
-                                style={"w-[100%] sm:w-[33%]"}
-                                value={dadosPaciente.cpf}
-                                onChange={handleChangeCPF}
-                                height="h-[45px]"
+                                    name={"CPF"}
+                                    style={"w-[100%] sm:w-[33%]"}
+                                    value={dadosPaciente.cpf}
+                                    onChange={handleChangeCPF}
+                                    height="h-[45px]"
                                 />
                             </div>
 
                             <div className="w-full flex flex-col gap-3 sm:flex-row sm:justify-between">
                                 <Input
-                                name={"DATA DE NASCIMENTO"}
-                                style={"w-[100%] sm:w-[33%]"}
-                                value={dadosPaciente.birthDate}
-                                onChange={handleChangeBirthdate}
-                                height="h-[45px]"
+                                    name={"DATA DE NASCIMENTO"}
+                                    style={"w-[100%] sm:w-[33%]"}
+                                    value={dadosPaciente.birthDate}
+                                    onChange={handleChangeBirthdate}
+                                    height="h-[45px]"
                                 />
 
                                 <Dropdown
-                                name={"SEXO"}
-                                style={"w-[100%] sm:w-[33%]"}
-                                options={sexoOptions}
-                                onChange={handleChangeSexo}
-                                height="h-[45px]"
+                                    name={"SEXO"}
+                                    style={"w-[100%] sm:w-[33%]"}
+                                    options={sexoOptions}
+                                    onChange={handleChangeSexo}
+                                    height="h-[45px]"
                                 />
 
                                 <Dropdown
-                                name={"PACIENTE É ESTUDANTE?"}
-                                style={"w-[100%] sm:w-[33%]"}
-                                options={isStudentOptions}
-                                onChange={handleChangeIsStudent}
-                                height="h-[45px]"
+                                    name={"PACIENTE É ESTUDANTE?"}
+                                    style={"w-[100%] sm:w-[33%]"}
+                                    options={isStudentOptions}
+                                    onChange={handleChangeIsStudent}
+                                    height="h-[45px]"
                                 />
                             </div>
 
                             <div className="w-full flex flex-col gap-3 sm:flex-row sm:justify-between">
                                 <Input
-                                name={"NACIONALIDADE"}
-                                style={"w-[100%] sm:w-[33%]"}
-                                value={dadosPaciente.nationality}
-                                onChange={handleChangeNationality}
-                                height="h-[45px]"
+                                    name={"NACIONALIDADE"}
+                                    style={"w-[100%] sm:w-[33%]"}
+                                    value={dadosPaciente.nationality}
+                                    onChange={handleChangeNationality}
+                                    height="h-[45px]"
                                 />
 
                                 <Input
-                                name={"NATURALIDADE"}
-                                style={"w-[100%] sm:w-[33%]"}
-                                value={dadosPaciente.naturalness}
-                                onChange={handleChangeNaturalness}
-                                height="h-[45px]"
+                                    name={"NATURALIDADE"}
+                                    style={"w-[100%] sm:w-[33%]"}
+                                    value={dadosPaciente.naturalness}
+                                    onChange={handleChangeNaturalness}
+                                    height="h-[45px]"
                                 />
 
                                 <Dropdown
-                                name={"BENEFICIÁRIO DO BOLSA FAMÍLIA"}
-                                style={"w-[100%] sm:w-[33%]"}
-                                options={bolsaFamiliaOptions}
-                                onChange={handleChangeBolsaFamilia}
-                                height="h-[45px]"
+                                    name={"BENEFICIÁRIO DO BOLSA FAMÍLIA"}
+                                    style={"w-[100%] sm:w-[33%]"}
+                                    options={bolsaFamiliaOptions}
+                                    onChange={handleChangeBolsaFamilia}
+                                    height="h-[45px]"
                                 />
                             </div>
                             <div className="w-full flex flex-col gap-3 sm:flex-row sm:justify-between">
                                 <Input
-                                name={"TIPO DE DEFICIÊNCIA"}
-                                style={"w-[100%] sm:w-[33%]"}
-                                value={dadosPaciente.typeOfDisability}
-                                onChange={handleChangeTypeOfDisability}
-                                height="h-[45px]"
+                                    name={"TIPO DE DEFICIÊNCIA"}
+                                    style={"w-[100%] sm:w-[33%]"}
+                                    value={dadosPaciente.typeOfDisability}
+                                    onChange={handleChangeTypeOfDisability}
+                                    height="h-[45px]"
                                 />
 
                                 <Input
-                                name={"TIPO DE CASA"}
-                                style={"w-[100%] sm:w-[33%]"}
-                                value={dadosPaciente.typeOfHousing}
-                                onChange={handleChangeTypeOfHousing}
-                                height="h-[45px]"
+                                    name={"TIPO DE CASA"}
+                                    style={"w-[100%] sm:w-[33%]"}
+                                    value={dadosPaciente.typeOfHousing}
+                                    onChange={handleChangeTypeOfHousing}
+                                    height="h-[45px]"
                                 />
 
                                 <Input
-                                name={"NÚMERO DO NIS"}
-                                style={"w-[100%] sm:w-[33%]"}
-                                value={dadosPaciente.nis}
-                                onChange={handleChangeNumeroNis}
-                                height="h-[45px]"
+                                    name={"NÚMERO DO NIS"}
+                                    style={"w-[100%] sm:w-[33%]"}
+                                    value={dadosPaciente.nis}
+                                    onChange={handleChangeNumeroNis}
+                                    height="h-[45px]"
                                 />
                             </div>
 
-                            
-                            <div className={`${erro == "..."? 'text-white': 'text-red-600'}`}>{erro}</div>
 
-                            
+                            <div className={`${erro == "..." ? 'text-white' : 'text-red-600'}`}>{erro}</div>
+
+
 
                             <button
-                            className="w-[80%] h-[60px] bg-[#4B8A89] text-white rounded-md flex justify-center items-center text-sm font-bold"
-                            onClick={clickProximaEtapa}
+                                className="w-[80%] h-[60px] bg-[#4B8A89] text-white rounded-md flex justify-center items-center text-sm font-bold"
+                                onClick={clickProximaEtapa}
                             >
-                            PRÓXIMA ETAPA
+                                PRÓXIMA ETAPA
                             </button>
 
                         </div>
