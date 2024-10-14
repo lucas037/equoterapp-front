36a23d6f13649ae89e7a71572b18da95145b9844
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-10-14 13:38:33 -0300

    update: Correção do fluxo do header na tela inicial
    This commit updates the button text and route in the header component. The buttonExtraName has been changed from 'Cadastro' to 'Entrar', and the routeExtra has been changed from '/cadastro' to '/login'.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,8 +33,8 @@ export default function Home() {
 
       <Header
       userNotLogged={true}
-      buttonExtraName='Cadastro'
-      routeExtra='/cadastro'
+      buttonExtraName='Entrar'
+      routeExtra='/login'
       />
 
 
